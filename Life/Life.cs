d0163d2aef14fb3c92305bf5a205411e7200ecfc
Staces <staescedric@gmail.com>
2017-12-18 12:10:20 +0100

    Life and MaxLife correct

--- Life/Life.cs
+++ Life/Life.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Life: MonoBehaviour
 {
-	[Header("Life of Target")]
+	[Header("Life of Target (%)")]
 	[Range(0,10)]
-	[TooltipAttribute("Change target's life")]
-	public int m_life;
+	[TooltipAttribute("Change target's life in pourcent")]
+	public int m_life = 10;
 
-	public int TheLife{
+	public int TheLife
+	{
 		get{ return m_life; }
 		set{ m_life = Mathf.Clamp(value,0,10); }
+	}
+
+	public int GetTargetLife(int _percent, int _maxPercent)
+	{
+		return (_maxPercent*(_percent*10))/100;
+	}
+
+	public void isDead(GameObject _object)
+	{
+		Debug.Log(_object+" is Dead");
+	}
+
+	public void isAlive(GameObject _object)
+	{
+		Debug.Log(_object+" is Alive");
 	}
 }
--- Life/LifeTarget.cs
+++ Life/LifeTarget.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class LifeTarget : Life
 {
-	
+	[Header("Target's Params")]
+	public int m_maxLife = 50;
+
+	[Header("Debug")]
+	public int d_life;
+
+	void Awake()
+	{}
+
+	void Update()
+	{
+		m_targetLife = GetTargetLife(m_life, m_maxLife);
+		if(m_targetLife <= 0)
+			isDead(gameObject);
+		else
+			isAlive(gameObject);
+	} 
+
+	void OnValidate()
+	{
+		d_life = GetTargetLife(m_life, m_maxLife);
+	}
+
+	private int m_targetLife;
 }
