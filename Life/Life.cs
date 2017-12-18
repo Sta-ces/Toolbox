442ee0dd287fd5d00168745c24987f6e46efa3fc
Staces <staescedric@gmail.com>
2017-12-18 10:58:39 +0100

    Initialized

---
+++ Life/Life.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life: MonoBehaviour
+{
+	[Header("Life of Target")]
+	[Range(0,10)]
+	[TooltipAttribute("Change target's life")]
+	public int m_life;
+
+	public int TheLife{
+		get{ return m_life; }
+		set{ m_life = Mathf.Clamp(value,0,10); }
+	}
+}
