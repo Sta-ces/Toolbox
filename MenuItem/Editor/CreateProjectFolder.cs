86a39520763f1bff4ff2be288b141a263f4d56de
Staces <staescedric@gmail.com>
2017-12-19 10:13:33 +0100

    Createfolder Editor

---
+++ MenuItem/Editor/CreateProjectFolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateProjectFolder : Editor
+{
+    [MenuItem("ToolBox/Create/Project")]
+    public static void CreateProject()
+    {
+        string[] Folders = { "_Scenes", "Scripts", "Materials" };
+        foreach (string folder in Folders)
+        {
+            AssetDatabase.CreateFolder("Assets/Test", folder);
+        }
+        AssetDatabase.Refresh();
+    }
+}
